b96d98510be3efac06bbfaa90b13c91184adf9db
unknown <kevin199932@gmail.com>
2024-01-27 16:33:55 +0800

    Updated fitz, adapting for cs papers

--- preprocess.py
+++ preprocess.py
@@ -1,27 +1,27 @@
 from PyPDF2 import PdfReader, PdfWriter
-from fitz import fitz
+import fitz
 import pdfminer
 from pdfminer.high_level import extract_text
 import re
 import os
 import pandas as pd
 from tqdm import tqdm
+# import camelot
 
 
 
-
-def read_pdf(file_path):
+def read_pdf(file_path, paper_type):
     text = ""
     # Open the PDF document
     try:
         doc = fitz.open(file_path)
         # print(file_path)
         doc.delete_page(0)
-        
+        if paper_type == "ms":
+            doc.delete_page(0)
         # Access a specific page
         for page in doc:
             text += page.get_text()
-            # page.set_cropbox(crop_box)
     except fitz.FileNotFoundError:
         return False
 
@@ -30,20 +30,30 @@ def read_pdf(file_path):
 
 #     doc.save(output_file_path)
 
-def remove_text(text):
+def remove_text(text, paper_type):
     # text = extract_text(output_file_path)
+    if paper_type == "qp":
+        text = re.sub(r"\.{2,}", "", text)
+        text = re.sub(r"\s+BLANK PAGE\s+", "", text)
 
-    text = re.sub(r"\.{2,}", "", text)
-    text = re.sub(r"\s+BLANK PAGE\s+", "", text)
+        text = re.sub(r"\[Turn over", "", text)
 
-    text = re.sub(r"\[Turn over", "", text)
+        pattern = r'\d{2}_\d{4}_\d{2}_\d{4}_\d+(\.\d+)?(?:[^\d]*\d*)?\n© UCLES \d+\n\d+'
+        # Use re.sub with re.DOTALL to replace matched patterns with an empty string
+        text = re.sub(pattern, '', text, flags=re.DOTALL)
+        
 
-    pattern = r'\d{2}_\d{4}_\d{2}_\d{4}_\d+(\.\d+)?(?:[^\d]*\d*)?\n© UCLES \d+\n\d+'
-    # Use re.sub with re.DOTALL to replace matched patterns with an empty string
-    text = re.sub(pattern, '', text, flags=re.DOTALL)
+        pattern = re.compile(r'^\d+$\n^\d+/\d+/[A-Z]/[A-Z]/\d+$\n^© UCLES \d{4}$', re.MULTILINE)
+        # Use re.sub() to replace the matched pattern with an empty string
+        text = re.sub(pattern, '', text)
 
-    text = '\n'.join(line for line in text.splitlines() if line.strip())
+        text = '\n'.join(line for line in text.splitlines() if line.strip())
+    if paper_type == "ms":
+        pattern = re.compile(r'Question\s*Answer\s*', re.MULTILINE)
 
+        # Use re.sub to remove the matched pattern
+        text = re.sub(pattern, '', text)
+        print(text)
     return text
 
 def divide_questions(text):
@@ -73,7 +83,6 @@ def define_question_no(questions):
             question = f'{match} {question}'
             questions[i] = question
 
-            print(i)
             if i != 0:
                 if match != "(a)":
                     question_no.append(question_no[i - 1])
@@ -93,19 +102,34 @@ def new_df(array, paper_no):
     df = pd.DataFrame({"Paper_No": paper_no, "Question Number": question_no, "Question": questions, "Marks": [value for i, value in enumerate(array) if i % 2 == 1]})
     return df
 
-# a = divide(read_pdf(delete_page1_header_footer()))[:-1]
-df = pd.DataFrame({"Paper_No": "", "Question Number": "", "Question": "", "Marks": []})
-for year in tqdm([19, 20, 21, 22, 23]):
-    for code in [11, 12, 13]:
-        for time in ['m', 's', 'w']:
-            # Define input and output file paths
-            # input_file_path = "pp/0417_m21_qp_12.pdf"
-            # output_file_path = "pp/0417_m21_qp_12_new.pdf"
-            paper_no = f"0417_{time}{year}_qp_{code}"
-            input_file_path = f"pp/{paper_no}.pdf"
-            if a := read_pdf(input_file_path):
-                # a = read_pdf(input_file_path) 
-                a = remove_text(a)
-                a = divide_questions(a)[:-1]
-                df = pd.concat([df, new_df(a, paper_no)])
-# df.to_excel('output.xlsx', index=False)
+def main():
+    df = pd.DataFrame({"Paper_No": "", "Question Number": "", "Question": "", "Marks": []})
+    for year in tqdm([19, 20, 21, 22, 23]):
+        for code in [11, 12, 13]:
+            for time in ['m', 's', 'w']:
+                # Define input and output file paths
+                # input_file_path = "pp/0417_m21_qp_12.pdf"
+                # output_file_path = "pp/0417_m21_qp_12_new.pdf"
+
+                paper_no = f"0478_{time}{year}_qp_{code}"
+
+                input_file_path = f"cs/qp/p1/{paper_no}.pdf"
+
+                if qp := read_pdf(input_file_path, "qp"):
+                    # a = read_pdf(input_file_path) 
+                    qp = remove_text(qp, "qp")
+                    qp = divide_questions(qp)[:-1]
+                    for a, i in enumerate(qp):
+                        print(i)
+                        print(f"{a}=======================================================================================================================")
+                    df = pd.concat([df, new_df(qp, paper_no)])
+    df.to_excel('output_cs.xlsx', index=False)
+
+main()
+
+# ms_input_file_path = "pp/0417_m21_ms_12.pdf"
+# # ms = read_pdf(ms_input_file_path, "ms")
+# # ms = remove_text(ms, "ms")
+# # print(ms)
+# a = camelot.read_pdf(ms_input_file_path, pages="7")
+# print(a[0].df)
