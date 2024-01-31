b9694003d58634cc0771e422391781a425659df9
unknown <kevin199932@gmail.com>
2024-01-31 18:45:42 +0800

    MS split

--- preprocess.py
+++ preprocess.py
@@ -1,9 +1,5 @@
-from PyPDF2 import PdfReader, PdfWriter
 import fitz
-import pdfminer
-from pdfminer.high_level import extract_text
 import re
-import os
 import pandas as pd
 from tqdm import tqdm
 # import camelot
@@ -47,28 +43,43 @@ def remove_text(text, paper_type):
         # Use re.sub() to replace the matched pattern with an empty string
         text = re.sub(pattern, '', text)
 
-        text = '\n'.join(line for line in text.splitlines() if line.strip())
     if paper_type == "ms":
-        pattern = re.compile(r'Question\s*Answer\s*', re.MULTILINE)
-
+        pattern = re.compile(r'Question\s*Answer\s*Marks\s*', re.MULTILINE)
         # Use re.sub to remove the matched pattern
         text = re.sub(pattern, '', text)
-        print(text)
+
+        # Define the pattern to match the text
+        pattern = re.compile(r'\d{4}/\d+\s*Cambridge IGCSE – Mark Scheme\s*PUBLISHED\s*March \d{4}')
+        # Use re.sub() to replace the matched pattern with an empty string
+        text = re.sub(pattern, '', text)
+
+        pattern = re.compile(r'© UCLES \d+\s*Page \d+ of \d+')
+
+        # Use re.sub() to replace the matched pattern with an empty string
+        text = re.sub(pattern, '', text)
+    text = '\n'.join(line for line in text.splitlines() if line.strip())
     return text
 
-def divide_questions(text):
+def divide_questions(text, paper_type):
 
-    # Use regular expression to split text based on the pattern "[number]"
-    split_text = re.split(r'(\[\d+\])', text)
+    if paper_type == "qp":
+        # Use regular expression to split text based on the pattern "[number]"
+        split_text = re.split(r'(\[\d+\])', text)
+        result = [item for item in split_text if item]
+    elif paper_type == "ms":
+        pattern = re.compile(r'\d+ \n')
+        result = re.split(pattern, text)
+        result = [line.strip() for line in result if line.strip()]
 
     # Filter out empty strings and keep square brackets at the end
-    result = [item for item in split_text if item]
+
 
     return result
 
 def define_question_no(questions):
     # Define a pattern to match letters inside brackets
-    pattern = re.compile(r'\(([a-zA-Z])\)')
+    pattern = re.compile(r'\((?:[a-d]|(?:i{1,3}|iv))\)')
+
     question_no = [1]
     for i, question in enumerate(questions):
         # Find all matches in the input string
@@ -102,34 +113,54 @@ def new_df(array, paper_no):
     df = pd.DataFrame({"Paper_No": paper_no, "Question Number": question_no, "Question": questions, "Marks": [value for i, value in enumerate(array) if i % 2 == 1]})
     return df
 
-def main():
+def extract_qp() -> None:
     df = pd.DataFrame({"Paper_No": "", "Question Number": "", "Question": "", "Marks": []})
     for year in tqdm([19, 20, 21, 22, 23]):
         for code in [11, 12, 13]:
-            for time in ['m', 's', 'w']:
-                # Define input and output file paths
-                # input_file_path = "pp/0417_m21_qp_12.pdf"
-                # output_file_path = "pp/0417_m21_qp_12_new.pdf"
-
+            for time in ['s', 'm', 'w']:
+                #CS Paper
                 paper_no = f"0478_{time}{year}_qp_{code}"
-
                 input_file_path = f"cs/qp/p1/{paper_no}.pdf"
 
+                #ICT Paper
+                paper_no = f"0417_{time}{year}_qp_{code}"
+                input_file_path = f"ict/qp/{paper_no}.pdf"
+
                 if qp := read_pdf(input_file_path, "qp"):
-                    # a = read_pdf(input_file_path) 
                     qp = remove_text(qp, "qp")
-                    qp = divide_questions(qp)[:-1]
-                    for a, i in enumerate(qp):
-                        print(i)
-                        print(f"{a}=======================================================================================================================")
-                    df = pd.concat([df, new_df(qp, paper_no)])
+                    qp = divide_questions(qp, "qp")
+                    if len(qp) % 2 == 1:
+                        qp = qp[:-1]
+                    try:    
+                        df = pd.concat([df, new_df(qp, paper_no)])
+                    except ValueError:
+                        print(f"{paper_no} not able to add to dataframe")
     df.to_excel('output_cs.xlsx', index=False)
+    df.to_json('output.json', orient="records")
 
-main()
+def extract_ms() -> None:
+    # for year in tqdm([19, 20, 21, 22, 23]):
+    #     for code in [11, 12, 13]:
+    #         for time in ['s', 'm', 'w']:
+                #CS Paper
+                # paper_no = f"0478_{time}{year}_ms_{code}"
+                # input_file_path = f"cs/ms/p1/{paper_no}.pdf"
 
-# ms_input_file_path = "pp/0417_m21_ms_12.pdf"
-# # ms = read_pdf(ms_input_file_path, "ms")
-# # ms = remove_text(ms, "ms")
-# # print(ms)
-# a = camelot.read_pdf(ms_input_file_path, pages="7")
-# print(a[0].df)
+                # #ICT Paper
+                # paper_no = f"0417_{time}{year}_ms_{code}"
+                # input_file_path = f"ict/ms/{paper_no}.pdf"
+
+                # if ms := read_pdf(input_file_path, "ms"):
+                #     ms = remove_text(ms, "ms")
+                #     ms = 
+
+    input_file_path = f"ict/ms/0417_m21_ms_12.pdf"
+    ms = read_pdf(input_file_path, "ms")
+    ms = remove_text(ms, "ms")
+    ms = divide_questions(ms, "ms")
+    for i in ms:
+        print("===")
+        print(i)
+
+# extract_qp()
+extract_ms()
